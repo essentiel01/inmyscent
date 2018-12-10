0e08fa20e6e48ed0ecd14ed29cfadca85ddb4673
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-12-10 02:13:11 +0100

    ajout des relations entre les entities

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -35,6 +37,27 @@ class Product
      * @ORM\Column(type="datetime", nullable=true)
      */
     private $created_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Brand", inversedBy="products")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $topNotes;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\FamilyNote", inversedBy="products")
+     */
+    private $familyNote;
+
+    public function __construct()
+    {
+        $this->familyNote = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -85,6 +108,56 @@ class Product
     public function setCreatedAt(?\DateTimeInterface $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getTopNotes(): ?string
+    {
+        return $this->topNotes;
+    }
+
+    public function setTopNotes(string $topNotes): self
+    {
+        $this->topNotes = $topNotes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|FamilyNote[]
+     */
+    public function getFamilyNote(): Collection
+    {
+        return $this->familyNote;
+    }
+
+    public function addFamilyNote(FamilyNote $familyNote): self
+    {
+        if (!$this->familyNote->contains($familyNote)) {
+            $this->familyNote[] = $familyNote;
+        }
+
+        return $this;
+    }
+
+    public function removeFamilyNote(FamilyNote $familyNote): self
+    {
+        if ($this->familyNote->contains($familyNote)) {
+            $this->familyNote->removeElement($familyNote);
+        }
 
         return $this;
     }
