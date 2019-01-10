388438e0e4e1a01398671cc0501889a13b523871
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-01-10 01:10:53 +0100

    requete ajax de l'autocompletion de l'input brand et gestion des ereurs

--- public/js/autocompletion.js
+++ public/js/autocompletion.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
+
+  /* ----------------
+    autocompletion du champ brand
+    ---------------------------*/
+  var brands = [];
+    // requete ajax
+    $.get( "http://localhost/inmyscent/public/index.php/get-brands/all/", function( data ) {
+
+    if (data.success == undefined) {
+        $( data ).each(function( index , value) {
+          brands.push(value.name);
+        });
+      }
+
+      if (data.success == false) {
+        $('#brands').focus(function(e) {
+          var div = $(this).after('<div class="alert alert-danger" id="error-message"></div>');
+          $(this).parent().children('div').text(data.message);
+        });
+
+        $('#brands').blur(function(e) {
+          $(this).parent().children('#error-message').remove();
+        });
+      }
+    });
+
+    // autocompletion
     $( function() {
-        var brands = [
-          "ActionScript",
-          "AppleScript",
-          "Asp",
-          "BASIC",
-          "C",
-          "C++",
-          "Clojure",
-          "COBOL",
-          "ColdFusion",
-          "Erlang",
-          "Fortran",
-          "Groovy",
-          "Haskell",
-          "Java",
-          "JavaScript",
-          "Lisp",
-          "Perl",
-          "PHP",
-          "Python",
-          "Ruby",
-          "Scala",
-          "Scheme"
-        ];
         $( "#brands" ).autocomplete({
           source: brands
         });
-      } );
+    });
+
+    
 });
