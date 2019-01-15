d468339fcbd2966f2de40faf40670f8993d8cfe4
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-01-15 03:20:44 +0100

    nouveau fichier de migration

---
+++ src/Migrations/Version20190115004310.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190115004310 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE brand ADD slug VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE brand DROP slug');
+        $this->addSql('ALTER TABLE product DROP slug');
+    }
+}
