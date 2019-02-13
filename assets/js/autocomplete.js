cc713e60684b4ec60f464debde6b934d6f201ed1
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-02-14 00:13:26 +0100

    installation de webpack encore et de yarn

---
+++ assets/js/autocomplete.js
@@ -0,0 +1,115 @@
+$(document).ready(function() {
+    /* ----------------
+      autocompletion du champ brand
+      ---------------------------*/
+    var brandsName = [];
+    var brands = [];
+      // requete ajax
+      $.get( "././index.php/brands", function( data ) {
+        if (data.success == undefined) {
+          $( data ).each(function( index , value) {
+            brands.push(value);
+            brandsName.push(value.name);
+          });
+        }
+  
+        if (data.success == false) {
+          $(this).focus(function(e) {
+            var div = $(this).after('<div class="alert alert-danger" id="brand-error-message"></div>');
+            $(this).parent().children('div').text(data.message);
+          });
+  
+          $('#brand').blur(function(e) {
+            $(this).parent().children('#brand-error-message').remove();
+          });
+        }
+      });
+  
+  
+      
+  
+      // autocompletion
+      $( function() {
+          $( "#brand" ).autocomplete({
+            source: brandsName
+          });
+      }); 
+  
+  
+      /* ----------------
+      autocompletion du champ product
+      ---------------------------*/
+      var productsName = [];
+  
+      $("#brand").change(function(){
+        
+          var brandName = $("#brand").val();
+          brandName = brandName.trim();
+  
+          if (brandName) {
+            $.ajax(
+                {
+                    url: "././index.php/products",
+                    method: "POST",
+                    dataType: "json",
+                    data: { brandName: brandName }
+                }
+            ).done(function(data) {
+  
+              if (data.success == undefined) {
+                // vide les variables products et productsName
+                productsName.splice(0, productsName.length);
+    
+                // assigne les nouvelles valeurs à products et productsName
+                $( data ).each(function( index , value) {
+                  if (productsName.includes(value.name) === false)
+                  {
+                    productsName.push(value.name);
+                  }
+                });
+  
+              } else if (data.success == false && data.type == 'fail') {
+  
+                  var div = $('#product').after('<div class="alert alert-warning" id="product-error-message"></div>');
+                  $('#product').parent().children('div').text(data.message);
+    
+                  $('#product').focus(function(e) {
+                  var div = $(this).after('<div class="alert alert-warning" id="product-error-message"></div>');
+                  $(this).parent().children('div').text(data.message);
+                  });
+        
+                  $('#product').blur(function(e) {
+                    $(this).parent().children('#product-error-message').remove();
+                  });
+  
+              } else if (data.success == false && data.type == 'not found') {
+                // vide les variables products et productsName
+                productsName.splice(0, productsName.length);
+              }
+  
+            }).fail(function(error) {
+                console.log(error);
+            });
+            
+        } else {
+          // vide les variables products et productsName
+          productsName.splice(0, productsName.length);
+        }
+        
+         
+          // autocompletion
+          $( function() {
+            $( "#product" ).autocomplete({
+              source: productsName
+            });
+          });
+  
+      });
+      
+      
+  
+      
+  
+      
+      
+  });
