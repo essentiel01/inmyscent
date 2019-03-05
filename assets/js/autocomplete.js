4f898d2ed812c1431df63f90c0025965d1170c2e
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-03-06 00:25:55 +0100

    function d'autocompletion de l'input brand du formulaire search by note

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
       ---------------------------*/
     var brandsName = [];
     var brands = [];
- 
-  
+
+
        // requete ajax
        $.get( "././index.php/brands", function( data ) {
          if (data.success == undefined) {
@@ -14,13 +14,13 @@ $(document).ready(function() {
              brandsName.push(value.name);
            });
          }
-   
+
          if (data.success == false) {
            $(this).focus(function(e) {
              var div = $(this).after('<div class="alert alert-danger" id="brand-error-message"></div>');
              $(this).parent().children('div').text(data.message);
            });
-   
+
            $("#brand").blur(function(e) {
              $(this).parent().children('#brand-error-message').remove();
            });
@@ -32,22 +32,29 @@ $(document).ready(function() {
          $( "#search-by-name #brand" ).autocomplete({
            source: brandsName
          });
-       }); 
+       });
 
        // autocompletion du champ brand du formualaire search by family note
        $( function() {
          $( "#search-by-family-note #brand" ).autocomplete({
            source: brandsName
          });
-       }); 
-  
+       });
+
+       // autocompletion du champ brand du formualaire search by note
+       $( function() {
+         $( "#search-by-note #brand" ).autocomplete({
+           source: brandsName
+         });
+       });
+
     /* ----------------
     autocompletion du champ product
     ---------------------------*/
     var productsName = [];
 
     $("#search-by-name #brand").change(function(){
-      
+
         var brandName = $("#search-by-name #brand").val();
         brandName = brandName.trim();
 
@@ -64,7 +71,7 @@ $(document).ready(function() {
             if (data.success == undefined) {
               // vide les variables products et productsName
               productsName.splice(0, productsName.length);
-  
+
               // assigne les nouvelles valeurs à products et productsName
               $( data ).each(function( index , value) {
                 if (productsName.includes(value.name) === false)
@@ -77,12 +84,12 @@ $(document).ready(function() {
 
                 var div = $('#search-by-name #product').after('<div class="alert alert-warning" id="product-error-message"></div>');
                 $('#search-by-name #product').parent().children('div').text(data.message);
-  
+
                 $('#search-by-name #product').focus(function(e) {
                 var div = $(this).after('<div class="alert alert-warning" id="product-error-message"></div>');
                 $(this).parent().children('div').text(data.message);
                 });
-      
+
                 $('#search-by-name #product').blur(function(e) {
                   $(this).parent().children('#product-error-message').remove();
                 });
@@ -95,12 +102,12 @@ $(document).ready(function() {
           }).fail(function(error) {
               console.log(error);
           });
-          
+
       } else {
         // vide les variables products et productsName
         productsName.splice(0, productsName.length);
       }
-        
+
         // autocompletion
         $( function() {
           $( "#search-by-name #product" ).autocomplete({
@@ -109,9 +116,9 @@ $(document).ready(function() {
         });
 
     });
-      
-      
-  
+
+
+
     /* ----------------
     autocompletion du champ family note
     ---------------------------*/
@@ -119,7 +126,7 @@ $(document).ready(function() {
     var familyNotes = [];
 
     $("#search-by-family-note #brand").change(function(){
-      
+
         var brandName = $("#search-by-family-note #brand").val();
         brandName = brandName.trim();
 
@@ -136,7 +143,7 @@ $(document).ready(function() {
               console.log(data)
               // vide la variable familyNote
               familyNotes.splice(0, familyNotes.length);
-  
+
               // assigne les nouvelles valeurs à la variable familyNote
               $( data ).each(function( index , value) {
                 if (familyNotes.includes(value.familyNotes) === false)
@@ -149,12 +156,12 @@ $(document).ready(function() {
 
                 var div = $('#search-by-family-note #family-note').after('<div class="alert alert-warning" id="product-error-message"></div>');
                 $('#search-by-family-note #family-note').parent().children('div').text(data.message);
-  
+
                 $('#search-by-family-note #family-note').focus(function(e) {
                 var div = $(this).after('<div class="alert alert-warning" id="product-error-message"></div>');
                 $(this).parent().children('div').text(data.message);
                 });
-      
+
                 $('#search-by-family-note #family-note').blur(function(e) {
                   $(this).parent().children('#product-error-message').remove();
                 });
@@ -167,12 +174,12 @@ $(document).ready(function() {
           }).fail(function(error) {
               console.log(error);
           });
-          
+
       } else {
         // vide la variable familyNote
         familyNotes.splice(0, familyNotes.length);
       }
-        
+
       // autocompletion de l'input family-note
       $( function() {
         $( "#search-by-family-note #family-note" ).autocomplete({
@@ -180,7 +187,7 @@ $(document).ready(function() {
         });
       });
     });
-  
-      
-      
+
+
+
   });
