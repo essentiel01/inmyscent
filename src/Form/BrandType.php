6d1f39a555b1bf017eb5f64a79f5071541503d76
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-03-08 13:06:35 +0100

    Ajout des contraintes de validation aux formulaire

--- src/Form/BrandType.php
+++ src/Form/BrandType.php
@@ -6,14 +6,28 @@ use App\Entity\Brand;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+// types
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+// validation
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+
 
 class BrandType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name', TextType::class, 
+                [
+                    "label" => "Nom de la marque", 
+                    "constraints" => [
+                        new NotBlank(["message"=>"Ce champ ne peut être vide"]),
+                        new Length(["min"=> 3, "minMessage"=>"le nom de la marque doit contenir au moins {{ limit }} caractères"])
+                        ]
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,26 +2,125 @@
 
 namespace App\Form;
 
-use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use App\Entity\Product;
+use App\Entity\Brand;
+
+use Doctrine\ORM\EntityRepository;
+
+// types
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+// validation
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('gender')
-            ->add('type')
-            ->add('created_at')
-            ->add('topNotes')
-            ->add('familyNotes')
-            ->add('baseNotes')
-            ->add('heartNotes')
-            ->add('notes')
-            ->add('brand')
+            ->add('name', TextType::class, 
+                [
+                    "label" => "Nom du parfum", 
+                    "constraints" => [
+                        new NotBlank(["message" => "Ce champ ne peut être vide"]),
+                        new Length([
+                            "min" => 2, 
+                            "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                        ])
+                    ]
+                ]
+            )
+            ->add('gender', ChoiceType::class,
+                [
+                    "label" => "Genre",
+                    "choices" => [
+                        "Masculin" => "m",
+                        "Féminin" => "f",
+                        "Mixte" => "mixte"
+                    ],
+                    "constraints" => new NotBlank(["message" => "Ce champ ne peut être vide"])
+                ]
+            )
+            ->add('type', TextType::class, 
+                [
+                    "label" => "Concentration",
+                    "constraints" => [
+                        new NotBlank(["message" => "Ce champ ne peut être vide"]),
+                        new Length([
+                            "min" => 3, 
+                            "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                        ])
+                    ]
+                ]
+            )
+            ->add('created_at', DateTimeType::class,
+                [
+                    "label" => "Date d'enregistrement",
+                    "required" => FALSE,
+                    "date_widget" => "single_text"
+                ]
+            )
+            ->add('topNotes', TextType::class,
+                [
+                    "label" => "Notes de tête",
+                    "required" => FALSE
+                ]
+            )
+            ->add('familyNotes', TextType::class,
+                [
+                    "label" => "Famille de notes",
+                    "constraints" => new Length([
+                        "min" => 3,
+                        "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                    ])
+                ]
+            )
+            ->add('baseNotes', TextType::class,
+                [
+                    "label" => "Notes de base",
+                    "required" => FALSE,
+                    "constraints" => new Length([
+                        "min" => 3,
+                        "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                    ])
+                ]
+            )
+            ->add('heartNotes', TextType::class,
+                [
+                    "label" => "Notes de coeur",
+                    "required" => FALSE,
+                    "constraints" => new Length([
+                        "min" => 3,
+                        "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                    ])
+                ]
+            )
+            ->add('notes', TextType::class,
+                [
+                    "label" => "Notes",
+                    "required" => FALSE,
+                    "constraints" => new Length([
+                        "min" => 3,
+                        "minMessage" => "Ce champ doit contenir au moins {{ limit }} caractères"
+                    ])
+                ]
+            )
+            ->add('brand', EntityType::class, 
+                [
+                    "choice_label" => "name",
+                    "class" => Brand::class,
+                    "placeholder" => "Sélectionner une marque",
+                    "constraints" => new NotBlank(["message" => "Ce champ ne peut être vide"])
+                ]
+            )
         ;
     }
 
