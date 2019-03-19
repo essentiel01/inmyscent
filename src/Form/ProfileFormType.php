607c7ad0e700eaed137f3b383074d47e0082b5ee
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-03-19 16:36:24 +0100

    override de profileformtype.php  et des dossiers views/resetting et views/Changepassword

---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ProfileFormType extends AbstractType
+{
+    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->buildUserForm($builder, $options);
+
+        $constraintsOptions = array(
+            'message' => 'Mot de passe actuel incorrecte',
+        );
+
+        if (!empty($options['validation_groups'])) {
+            $constraintsOptions['groups'] = array(reset($options['validation_groups']));
+        }
+
+        $builder->add('current_password', PasswordType::class, array(
+            'label' => 'Mot de passe actuel',
+            'translation_domain' => 'FOSUserBundle',
+            'mapped' => false,
+            'constraints' => array(
+                new NotBlank(),
+                new UserPassword($constraintsOptions),
+            ),
+            'attr' => array(
+                'autocomplete' => 'current-password',
+            ),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\User',
+            'csrf_token_id' => 'profile',
+        ));
+    }
+
+    // BC for SF < 3.0
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'fos_user_profile';
+    }
+
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, array('label' => 'Nom d\'utilisateur', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', EmailType::class, array('label' => 'Adresse email', 'translation_domain' => 'FOSUserBundle'))
+        ;
+    }
+}
