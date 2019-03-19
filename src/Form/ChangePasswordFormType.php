7c8f377bf2bc17a67a7d0070625b4b64949d455e
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-03-19 11:23:11 +0100

    override le fichier changepasswordformtype.php et les dossiers views/profile et views/security du fosuserbundle

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class ChangePasswordFormType extends AbstractType
+{
+    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $constraintsOptions = array(
+            'message' => 'Mot de passe actuel invalide',
+        );
+
+        if (!empty($options['validation_groups'])) {
+            $constraintsOptions['groups'] = array(reset($options['validation_groups']));
+        }
+
+        $builder->add('current_password', PasswordType::class, array(
+            'label' => 'Mot de passe actuel',
+            'translation_domain' => 'FOSUserBundle',
+            'mapped' => false,
+            'constraints' => array(
+                new NotBlank(),
+                new UserPassword($constraintsOptions),
+            ),
+            'attr' => array(
+                'autocomplete' => 'current-password',
+            ),
+        ));
+
+        $builder->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'options' => array(
+                'translation_domain' => 'FOSUserBundle',
+                'attr' => array(
+                    'autocomplete' => 'new-password',
+                ),
+            ),
+            'first_options' => array('label' => 'Nouveau mot de passe'),
+            'second_options' => array('label' => 'Confirmation du mot de passe'),
+            'invalid_message' => 'les deux mot de passe doivent être identiques',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\User',
+            'csrf_token_id' => 'change_password',
+        ));
+    }
+
+    // BC for SF < 3.0
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'fos_user_change_password';
+    }
+}
