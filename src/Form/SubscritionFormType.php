9dc7bf1f30b5ce24e55bb7b97c40d51609eadef4
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-03-26 20:32:28 +0100

    creation de subscritionFormType

---
+++ src/Form/SubscritionFormType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+// types
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+// validation
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+
+class SubscritionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('email', EmailType::class, array(
+            'label' => 'Email',
+            'constraints' => array(new NotBlank())
+        ))
+        ->add('sexe', ChoiceType::class, array(
+            'label' => 'Genre',
+            'choices' => array(
+                'Masculin' => 'm',
+                'Féminin' => 'f'
+            ),
+            'expanded' => true
+        ))
+        ->add('save', SubmitType::class, ['label' => 'S\'inscrire']);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
