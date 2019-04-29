c6a2b2a2fb3f1cae26bc6967cc9bcbd562349e3b
essentiel01 <tchekpo_cletus@yahoo.fr>
2019-04-29 21:22:11 +0200

    création de NewsletterController.php

---
+++ src/Controller/NewsletterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Mailchimp\Mailchimp;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class NewsletterController extends AbstractController
+{
+    public function subscribe()
+    {
+        
+    }
+}
